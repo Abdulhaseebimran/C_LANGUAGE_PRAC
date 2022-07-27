70e9fc5491586d009f7ce6edb11df714d385829b
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-28 00:54:28 +0500

    first commit

---
+++ mybio.cpp
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+{
+//clrscr();
+printf("Name	: Abdul haseeb imran \n");
+printf("Roll No#	: CSC-22S-009\n");
+printf("Semester	: 1st \n");
+printf("Batch	: 2022\n");
+printf("Department	: Computer Science (CS)\n");
+printf("University	: SMI University\n");
+//getch();
+return 0;
+}
+
